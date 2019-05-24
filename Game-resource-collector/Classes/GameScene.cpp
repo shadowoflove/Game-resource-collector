97c564cd750ab79cb970336e1ef35d943ab25c5c
Gaolooong <48149933+Gaolooong@users.noreply.github.com>
2019-05-24 16:48:45 +0800

    人物移动和移动中动画已做好
    修改了stop动作不能执行bug

--- Game-resource-collector/Classes/GameScene.cpp
+++ Game-resource-collector/Classes/GameScene.cpp
@@ -30,24 +30,33 @@ bool Game::init()
 	Vec2 origin = Director::getInstance()->getVisibleOrigin();
 	//////////添加自己的代码
 
+/*TMXTiledMap* tile_map = TMXTiledMap::create("");//添加瓦片地图
+	addChild(tile_map, 0, 100);//将瓦片地图放在第0层
 
-	auto hero = Sprite::create("hero.png");
-	hero->setPosition(Vec2(visibleSize.width / 2 , visibleSize.height / 2));
+	TMXObjectGroup* group =tile_map->getObjectGroup("");
+	ValueMap spawn_point = group->getObject("");
+
+	float spawn_x = spawn_point["x"].asFloat;
+	float spawn_y = spawn_point["y"].asFloat;*/
+
+
+	auto hero = Sprite::create("hero/hero.png");//添加英雄
+	hero->setPosition(Vec2(visibleSize.width / 2 + origin.x, visibleSize.height / 2 + origin.y));
 	this->addChild(hero,1);//将英雄放在第一层
 
 	this->scheduleUpdate();
 	
-
-	auto touch = EventListenerTouchOneByOne::create();
+	auto touch = EventListenerTouchOneByOne::create();//触摸事件
 	touch->setSwallowTouches(true);
 	touch->onTouchBegan = CC_CALLBACK_2(Game::touchBegin,this);
 	touch->onTouchMoved = CC_CALLBACK_2(Game::touchMove, this);
 	touch->onTouchEnded = CC_CALLBACK_2(Game::touchEnd, this);
 
-	EventDispatcher* touch_jianting = Director::getInstance()->getEventDispatcher();
+	EventDispatcher* touch_jianting = Director::getInstance()->getEventDispatcher();//触摸事件 监听器
 
 	touch_jianting->addEventListenerWithSceneGraphPriority(touch, hero);
 
+	
 
 
 	return true;
@@ -78,57 +87,51 @@ int getDirection(int x,int y)//获得 行走的 方向
 
     return direction;
 }
-Animate* create_Animate(int direction, const char* action)//建立动作类动画
+Animate* create_Animate(int direction, const char* action,float time)//建立动作类动画
 {
 	Animation* animation = Animation::create();
-	Vector <SpriteFrame*> frameArray;
-	if (action == "stop")//八个方向上的最后站立
-	{
-	     for (int i = 0; i < 2; i++)
-		{
-			/*__String* frame_name = __String::createWithFormat("hero/stop_%d.png", direction);
-			SpriteFrame* sprite_frame = SpriteFrameCache::getInstance()->
-											getSpriteFrameByName(frame_name->getCString());
-			animation->addSpriteFrame(sprite_frame);*/
-
-			/*auto* frameCache = SpriteFrameCache::getInstance();
-			auto* frame = frameCache->getSpriteFrameByName
-								(String::createWithFormat("stop_%d.png", direction)->getCString());
-			frameArray.pushBack(frame);
-			Animation* animation = Animation::createWithSpriteFrames(frameArray);*/
-
-			 char  file_name[20] = { 0 };
-			 sprintf(file_name, "stop_%d.png", direction);
-			 animation->addSpriteFrameWithFile(file_name);
-		 }
-	}
+	
+	//Vector <SpriteFrame*> frameArray;
 	if (action == "run")//奔跑时的动画
 	{
 		char  file_name[20] = { 0 };
 		for (int i = 0; i < 2; i++)
 		{
+			sprintf(file_name, "hero/run_%d_%d.png", direction, i);
+			animation->addSpriteFrameWithFile(file_name);
+
 			/*__String* frame_name = __String::createWithFormat("hero/run_%d_%d.png", direction,i);
 			SpriteFrame* sprite_frame = SpriteFrameCache::getInstance()->
 				getSpriteFrameByName(frame_name->getCString());
 				animation->addSpriteFrame(sprite_frame);*/
 
-			/*auto* frameCache = SpriteFrameCache::getInstance();
-			for (int i = 0; i < 2; i++)
-			{
-				auto* frame = frameCache->getSpriteFrameByName
-				(String::createWithFormat("run_%d_%d.png", direction, i)->getCString());
-				frameArray.pushBack(frame);
-				}
-			Animation* animation = Animation::createWithSpriteFrames(frameArray);*/
-
-			sprintf(file_name, "run_%d_%d.png", direction,i);
+				/*auto* frameCache = SpriteFrameCache::getInstance();
+				for (int i = 0; i < 2; i++)
+				{
+					auto* frame = frameCache->getSpriteFrameByName
+					(String::createWithFormat("run_%d_%d.png", direction, i)->getCString());
+					frameArray.pushBack(frame);
+					}
+				Animation* animation = Animation::createWithSpriteFrames(frameArray);*/
+		}
+	}
+		else//八个方向上的最后站立
+		{
+			//auto hero = static_cast<Sprite*>(event->getCurrentTarget());
+			//hero->stopAllActions();
+			char  file_name[20] = { 0 };
+			sprintf(file_name, "hero/stop_%d.png", direction);
 			animation->addSpriteFrameWithFile(file_name);
 		}
-		animation->setLoops(-1);
+		
 		animation->setDelayPerUnit(0.1f);
-	}
-	Animate* animate = Animate::create(animation);
-	return animate;
+		if(action=="stop")
+		animation->setLoops(1);
+		else
+			animation->setLoops(time/0.2);//几张图片的走路动画就/0.1*几
+
+		Animate* animate = Animate::create(animation);
+		return animate;
 }
 
 bool Game::touchBegin(Touch* touch, Event* event)
@@ -142,10 +145,9 @@ bool Game::touchBegin(Touch* touch, Event* event)
 	float juli = sqrt(pow(diff.x, 2) + pow(diff.y, 2));
 
 	auto* move_action = MoveTo::create(juli / iv, touch_position);
-	auto* run_animate = create_Animate(getDirection(diff.x,diff.y),"run");
-	auto* stop_action = create_Animate(getDirection(diff.x, diff.y), "stop");
-	//hero->runAction(MoveTo::create(juli /iv, touch_position));//间隔动作:移动
-	auto* sequence = Sequence::create(move_action, run_animate, NULL);
+	auto* run_animate = create_Animate(getDirection(diff.x,diff.y),"run", juli / iv);
+	auto* stop_animate = create_Animate(getDirection(diff.x, diff.y), "stop",0);
+	auto* sequence = Sequence::create(move_action, stop_animate, NULL);
 	hero->runAction(run_animate);
 	hero->runAction(sequence);
 	return true;
